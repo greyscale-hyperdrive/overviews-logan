b70701a16b345cf769af619a9d750659b4368704
Logan Hart <2loganhart@gmail.com>
2018-06-19 23:02:28 -0700

    Stubbed out api router, will implement when I start working on db methods

--- server/application.js
+++ server/application.js
@@ -4,8 +4,11 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const db = require('../database/index');
 
+const restaurantRoutes = require('./api/routes/restaurant');
+
 const app = express();
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -23,15 +26,20 @@ app.use('/restaurant/:restaurantId', express.static(path.join(__dirname, '../pub
 app.use('/overviewsBundle.js', express.static(path.join(__dirname, '../public/dist/bundle.js')));
 app.use('/images/star-rating.png', express.static(path.join(__dirname, '../public/images/star-rating.png')));
 
-app.get('/overviews/restaurant/:restaurantId/overview', (req, res) => {
-  db.retrieve(req.params.restaurantId, (err, results) => {
-    if (err && err.message.includes('Cast to number failed for value "NaN"')) {
-      res.status(400).json('Bad request');
-    } else if (err) {
-      res.status(500).json('Unable to retrieve overview data from database');
-    } else {
-      res.status(200).json(results);
-    }
+app.use('/overviews/restaurant/', restaurantRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
   });
 });
 
